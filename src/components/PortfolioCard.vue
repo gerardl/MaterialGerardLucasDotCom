02200f2b03f43d1f9312b35c9d394cd2e6ad7002
gerardl <Gerard@GHOME2>
2018-07-23 16:04:41 -0500

    continue basic theme. still stock images

---
+++ src/components/PortfolioCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card class="my-3" hover>
+    <v-card-media
+        class="white--text"
+        height="170px"
+        :src="post.imgUrl">
+        <v-container fill-height fluid>
+            <v-layout>
+                <v-flex xs12 align-end d-flex>
+                    <span class="headline">{{ post.title }}</span>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-card-media>
+    <v-card-text>
+        {{ post.content }}
+    </v-card-text>
+    <v-card-actions>
+        <v-btn icon>
+            <v-icon>fab fa-github</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn flat class="blue--text">More</v-btn>
+    </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioCard',
+  props: {
+    post: {}
+  },
+  data() {
+    return {
+        
+    }
+  },
+  created() {
+      
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/PortfolioHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="text-xs-center mb-2">
+        <v-avatar size="125px">
+            <img class="img-circle elevation-7 mb-1" src=".././assets/gl_pic.jpg">
+        </v-avatar>
+        <div class="headline"><span class="font-weight-bold">Gerard Lucas</span></div>
+        <div class="subheading text-xs-center grey--text pt-1 pb-3">Senior Software Developer and Project Manager</div>
+        <v-layout justify-space-between>
+            <v-btn flat color="primary" href="https://www.linkedin.com/in/gerard-lucas-981267a0" target="_blank">
+                <v-icon color="light-blue" left>fab fa-linkedin</v-icon>LinkedIn
+            </v-btn>
+            <v-btn flat color="primary" href="https://github.com/gerardl" target="_blank">
+                <v-icon color="black" left>fab fa-github</v-icon>Github
+            </v-btn>
+            <v-btn flat color="primary" href="mailto:glucas@conperio.com" target="_blank">
+                <v-icon color="grey darken-2" left>mail</v-icon>Mail
+            </v-btn>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    a {
+        text-decoration: none;
+    }
+</style>
