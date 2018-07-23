aec3ea0bc11bdfead8a10a01adaf576c39a8cacd
gerardl <Gerard@GHOME2>
2018-07-23 17:59:21 -0500

    fix contact icon spacing on small screens

--- src/components/PortfolioHeader.vue
+++ src/components/PortfolioHeader.vue
@@ -5,16 +5,22 @@
         </v-avatar>
         <div class="headline"><span class="font-weight-bold">Gerard Lucas</span></div>
         <div class="subheading text-xs-center grey--text pt-1 pb-3">Senior Software Developer and Project Manager</div>
-        <v-layout justify-space-between>
-            <v-btn flat color="primary" href="https://www.linkedin.com/in/gerard-lucas-981267a0" target="_blank">
-                <v-icon color="light-blue" left>fab fa-linkedin</v-icon>LinkedIn
-            </v-btn>
-            <v-btn flat color="primary" href="https://github.com/gerardl" target="_blank">
-                <v-icon color="black" left>fab fa-github</v-icon>Github
-            </v-btn>
-            <v-btn flat color="primary" href="mailto:glucas@conperio.com" target="_blank">
-                <v-icon color="grey darken-2" left>mail</v-icon>Mail
-            </v-btn>
+        <v-layout row wrap justify-space-between>
+            <v-flex xs12 sm4>
+                <v-btn flat color="primary" href="https://www.linkedin.com/in/gerard-lucas-981267a0" target="_blank">
+                    <v-icon color="light-blue" left>fab fa-linkedin</v-icon>LinkedIn
+                </v-btn>
+            </v-flex>
+            <v-flex xs12 sm4>
+                <v-btn flat color="primary" href="https://github.com/gerardl" target="_blank">
+                    <v-icon color="black" left>fab fa-github</v-icon>Github
+                </v-btn>
+            </v-flex>
+            <v-flex xs12 sm4>
+                <v-btn flat color="primary" href="mailto:glucas@conperio.com" target="_blank">
+                    <v-icon color="grey darken-2" left>mail</v-icon>Mail
+                </v-btn>
+            </v-flex>
         </v-layout>
     </div>
 </template>
