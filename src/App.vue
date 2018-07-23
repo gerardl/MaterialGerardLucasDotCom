c16c94c21d82c8691dbf72bddc1a74d150269360
gerardl <Gerard@GHOME2>
2018-07-23 14:31:28 -0500

    start portfolio from template

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,107 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
+  <v-app light>
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <v-layout row wrap align-center>
+          <v-flex xs12 md4>
+            <div class="text-xs-center">
+              <v-avatar size="125px">
+                <img
+                  class="img-circle elevation-7 mb-1"
+                  src="https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/lists/1.jpg"
+                >
+              </v-avatar>
+              <div class="headline">John <span style="font-weight:bold">Carter</span></div>
+              <div class="subheading text-xs-center grey--text pt-1 pb-3">Lorem ipsum dolor sit amet</div>
+              <v-layout justify-space-between>
+                <a href="javascript:;" class="body-2">Home</a>
+                <a href="javascript:;" class="body-2">About</a>
+                <a href="javascript:;" class="body-2">Github</a>
+                <a href="javascript:;" class="body-2">Other</a>
+              </v-layout>
+            </div>
+          </v-flex>
+          <v-flex xs12 md5 offset-md2>
+            <div v-for="post in posts" :key="post.title">
+              <v-card class="my-3" hover>
+                <v-card-media
+                  class="white--text"
+                  height="170px"
+                  :src="post.imgUrl"
+                >
+                  <v-container fill-height fluid>
+                    <v-layout>
+                      <v-flex xs12 align-end d-flex>
+                        <span class="headline">{{ post.title }}</span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-media>
+                <v-card-text>
+                  {{ post.content }}
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn icon class="red--text">
+                    <v-icon medium>fa-reddit</v-icon>
+                  </v-btn>
+                  <v-btn icon class="light-blue--text">
+                    <v-icon medium>fa-twitter</v-icon>
+                  </v-btn>
+                  <v-btn icon class="blue--text text--darken-4">
+                    <v-icon medium>fa-facebook</v-icon>
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn flat class="blue--text">Read More</v-btn>
+                </v-card-actions>
+              </v-card>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+    <v-footer class="secondary" app>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <div class="white--text ml-3">
+            Made with
+            <v-icon class="red--text">favorite</v-icon>
+            by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+            and <a class="white--text" href="https://github.com/vwxyzjn" target="_blank">Costa Huang</a>
+          </div>
+        </v-flex>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
-
+<script src="https://use.fontawesome.com/73c8e2621d.js"></script>
 <script>
-import HelloWorld from './components/HelloWorld'
+import Landing from './components/Landing'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing
   },
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.js',
+      posts: [
+          {
+            title: 'Fusce ullamcorper tellus',
+            content: 'Fusce ullamcorper tellus sed maximus rutrum. Donec imperdiet ultrices maximus. Donec non tellus non neque pellentesque fermentum. Aenean in pellentesque urna.',
+            imgUrl: 'https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/cards/drop.jpg'
+          },
+          {
+            title: 'Donec vitae suscipit lectus, a luctus diam.',
+            content: 'Donec vitae suscipit lectus, a luctus diam. Proin vitae felis gravida, lobortis massa sit amet, efficitur erat. Morbi vel ultrices nisi.',
+            imgUrl: 'https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/cards/docks.jpg'
+          },
+          {
+            title: 'Vestibulum condimentum quam',
+            content: 'At sagittis sapien vulputate. Vivamus laoreet lacus id magna rutrum dapibus. Donec vel pellentesque arcu. Maecenas mollis odio tempus felis elementum commodo.',
+            imgUrl: 'https://raw.githubusercontent.com/vuetifyjs/docs/dev/static/doc-images/cards/plane.jpg'
+          }
+        ]
     }
   }
 }
